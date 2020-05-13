3e75d480a5680f0315ebcfaa380e6484864cae6d
Kozlovtsev Konstantin <ko-sova@yandex.ru>
2020-05-13 11:18:28 +0300

    adds docstrings to bot.py

--- bot.py
+++ bot.py
@@ -12,13 +12,13 @@ class GameBot(telebot.TeleBot):
     Class that represents a TelegramBot
     that handles all games and players interactions
 
-    Attributes
-    ----------
-    game: dict of {chat_id: game}
+    :attribute games: dict of {chat_id: game}
     """
+
     def __init__(self, token):
         """
         Creates a bot
+
         :param token:
             token (str) for game bot, if you don't have one,
             you should go to @BotFather in telegram and
@@ -34,7 +34,7 @@ class GameBot(telebot.TeleBot):
         Registers handlers for bot.
         Handler is a function with '@bot.message_handler' descriptor,
         once created, bot will continuously accept special commands from players
-        and handle it
+        and handle it.
         """
         @self.message_handler(commands=['help'])
         def show_help(message):
@@ -72,47 +72,78 @@ class GameBot(telebot.TeleBot):
         def text_handler(message):
             self.text_handler(message)
 
-    def show_help(self, message):
-        hints = '''
-        /help - показывает это сообщение
-
-        /rules - правила игры (TODO)
-        /hint - карточка с кратким описанием и стоимостью карт
-
-        /create - создает полностью новую игру с пустым списком игроков
-        /join - добавляет игрока в игру
-        /start - после добавления всех игроков начинает игру
-        /newround - перезапускает игру с теми же игроками и настройками (TODO)
-
-        /cards - показывает все сброшенные с рук карты
-        /players - показывает всех оставшихся в игре игроков
-
-        /doubledeck - играть в 2 колоды: больше карт, больше народу, больше веселья!
-        '''
-        self.send_message(message.chat.id, hints)
-
     def get_game(self, chat_id):
+        """
+        Helper function to get game by chat_id
+        where it is playing, if game not found returns None
+
+        :param chat_id:
+            int, id of the chat where game is played
+        :return:
+            Game, if game was created in chat with given id,
+            else None
+        """
         if chat_id not in self.games.keys():
             self.send_message(chat_id, _("There no game in this chat yet"))
             return
 
         return self.games[chat_id]
 
-    @staticmethod
-    def show_hint(self, message):
+    def show_help(self, message):
+        """
+        Shows help in chat
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
+
         hints = '''
-    8. Принцесса (x1) - если вы сбрасываете эту карту, оказываетесь вне игры
-    7. Графиня (x1) - если на руке Принц или Король, то Графиню надо сбросить
-    6. Король (x1) - скинув Короля обменяйтесь картами с одним из игроков
-    5. Принц (x2) - выбранный вами игрок должен скинуть свою карту и взять новую
-    4. Служанка (x2) - сбросив Служанку получаете защиту на следующий круг
-    3. Барон (x2) - сравните свою карту с другим игроком, у кого значение меньше - тот вылетает из игры
-    2. Священник (x2) - позволяет посмотреть карту другого игрока
-    1. Стражница (x5) - назовите карту другого игрока (не стражницу), если угадаете - он вылетет из игры
+/help - показывает это сообщение
+
+/rules - правила игры (TODO)
+/hint - карточка с кратким описанием и стоимостью карт
+
+/create - создает полностью новую игру с пустым списком игроков
+/join - добавляет игрока в игру
+/start - после добавления всех игроков начинает игру
+/newround - перезапускает игру с теми же игроками и настройками (TODO)
+
+/cards - показывает все сброшенные с рук карты
+/players - показывает всех оставшихся в игре игроков
+
+/doubledeck - играть в 2 колоды: больше карт, больше народу, больше веселья!
+        '''
+        self.send_message(message.chat.id, hints)
+
+    def show_hint(self, message):
+        """
+        Shows hint (small copy of game rules) in chat
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
+        hints = '''
+8. Принцесса (x1) - если вы сбрасываете эту карту, оказываетесь вне игры
+7. Графиня (x1) - если на руке Принц или Король, то Графиню надо сбросить
+6. Король (x1) - скинув Короля обменяйтесь картами с одним из игроков
+5. Принц (x2) - выбранный вами игрок должен скинуть свою карту и взять новую
+4. Служанка (x2) - сбросив Служанку получаете защиту на следующий круг
+3. Барон (x2) - сравните свою карту с другим игроком, у кого значение меньше - тот вылетает из игры
+2. Священник (x2) - позволяет посмотреть карту другого игрока
+1. Стражница (x5) - назовите карту другого игрока (не стражницу), если угадаете - он вылетет из игры
     '''
         self.send_message(message.chat.id, hints)
 
     def create_game(self, message):
+        """
+        Creates game in chat from where message is came
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
         chat_id = message.chat.id
         if chat_id in self.games.keys():
             markup = types.ReplyKeyboardRemove(selective=False)
@@ -123,6 +154,15 @@ class GameBot(telebot.TeleBot):
         self.games[chat_id] = Game(self, chat_id)
 
     def double_deck(self, message):
+        """
+        Toggles second set of cards,
+        it can be useful if more than 5
+        players are in game
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
         chat_id = message.chat.id
         game = self.get_game(chat_id)
 
@@ -135,12 +175,19 @@ class GameBot(telebot.TeleBot):
 
         if not game.double_deck:
             game.double_deck = True
-            self.send_message(message.chat.id, _("The second deck is added"))
+            self.send_message(chat_id, _("The second deck is added"))
         else:
             game.double_deck = False
-            self.send_message(message.chat.id, _("The second deck is removed"))
+            self.send_message(chat_id, _("The second deck is removed"))
 
     def join_user(self, message):
+        """
+        Adds user who wrote this message to game
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
         chat_id = message.chat.id
         game = self.get_game(chat_id)
 
@@ -148,25 +195,34 @@ class GameBot(telebot.TeleBot):
             return
 
         if game.started:
-            self.send_message(message.chat.id, _("Game has been already started"))
+            self.send_message(chat_id, _("Game has been already started"))
             return
 
         username = message.from_user.username or message.from_user.first_name
         user_id = message.from_user.id
 
         if user_id in game.users:
-            self.send_message(message.chat.id, _("Player @{} already joined to game.").format(username))
+            self.send_message(chat_id, _("Player @{} already joined to game.").format(username))
             return
 
         game.users.add(User(username, user_id, self))
-        self.send_message(message.chat.id, 'Player @{} joined to game'.format(username))
+        self.send_message(chat_id, 'Player @{} joined to game'.format(username))
 
         if game.users.num_users() == 6 and not game.double_deck:
             game.double_deck = True
-            self.send_message(message.chat.id,
+            self.send_message(chat_id,
                              _("The number of players reached 6, the second deck is automatically added"))
 
     def start_game(self, message):
+        """
+        Starts game, and make first turn.
+        There is no way to change number of players or
+        toggle double deck if game is already started.
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
         chat_id = message.chat.id
         game = self.get_game(chat_id)
 
@@ -177,6 +233,13 @@ class GameBot(telebot.TeleBot):
         game.start_turn()
 
     def show_cards(self, message):
+        """
+        Shows all cards that was already played
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
         chat_id = message.chat.id
         game = self.get_game(chat_id)
 
@@ -187,9 +250,18 @@ class GameBot(telebot.TeleBot):
         unique_used_cards = np.unique(sorted(game.used_cards), return_counts=True)
         for num, card in enumerate(unique_used_cards[0]):
             used_cards += ' - {:10s} [{}]\n'.format(card.name, unique_used_cards[1][num])
-        self.send_message(message.chat.id, used_cards)
+        self.send_message(chat_id, used_cards)
 
     def show_users(self, message):
+        """
+        Shows all players, who is current dealer,
+        and who is currently protected by the 'Maid' card to chat
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        :return:
+        """
         chat_id = message.chat.id
         game = self.get_game(chat_id)
 
@@ -205,9 +277,18 @@ class GameBot(telebot.TeleBot):
                 users += '<<'
             users += '\n'
 
-        self.send_message(message.chat.id, users)
+        self.send_message(chat_id, users)
 
     def text_handler(self, message):
+        """
+        Handles all free-text messages, that are
+        used for moving between states
+
+        :param message:
+            telebot.types.Message, message that contains
+            info about chat where it was written and user who wrote it
+        """
+
         chat_id = message.chat.id
         game = self.get_game(chat_id)
 
@@ -217,7 +298,7 @@ class GameBot(telebot.TeleBot):
         if game.dealer is None:
             return
 
-        if message.chat.id == game.dealer.uid:
+        if chat_id == game.dealer.uid:
             if game.state is 'select_card' and \
                     message.text in [game.dealer.card.name, game.dealer.new_card.name]:
                 game.select_card(message.text)
